984e342deb0a88d364a1190c8adb0352fc0522fb
Toysin <toysin1203@hotmail.com>
2024-11-23 20:26:06 +0900

    define interface and basic apis

--- service/service.go
+++ service/service.go
@@ -6,35 +6,46 @@ import (
 	"github.com/ToySin/finance/portfolio"
 )
 
-type storage interface {
-	SavePortfolio(p *portfolio.Portfolio) error
-
-	// GetPortfolio returns the portfolio of the given date.
-	// - The date should include the year and month. Other fields are ignored.
-	// - If the portfolio does not exist, it returns an error.
-	GetPortfolio(date time.Time) (*portfolio.Portfolio, error)
-}
-
+// Service is a service for managing finance data.
 type Service struct {
-	storage storage
+	storage PortfolioStorage
 }
 
+// New creates a new Service.
 func New() *Service {
 	return &Service{}
 }
 
-func (s *Service) AddMeal(date time.Time, name string, price int) error {
+// CreatePortfolio creates a new portfolio for the given date. (year and month)
+// When creating a new portfolio, the last month's fixed expenses are copied to the new portfolio in default.
+// TODO(#4): Copy from genral fixed expenses setting instead of the last month's fixed expenses.
+func (s *Service) CreatePortfolio(date time.Time) error {
+	// If there is no portfolio for the last month,
+	// create a new portfolio without copying the last month's fixed expenses.
+	var fixedExpense []portfolio.Transaction
+	lastPortfolio, err := s.storage.GetPortfolio(date.AddDate(0, -1, 0))
+	if err == nil {
+		fixedExpense = lastPortfolio.Transactions[portfolio.FixedExpenseCategory]
+	}
+
+	p := portfolio.NewPortfolio(date)
+	p.Transactions[portfolio.FixedExpenseCategory] = fixedExpense
+	return s.storage.SavePortfolio(p)
+}
+
+// CreateTransaction creates a transaction to the portfolio of the given date.
+func (s *Service) CreateTransaction(date time.Time, c portfolio.Category, name string, amount portfolio.Amount, note string) error {
 	p, err := s.storage.GetPortfolio(date)
 	if err != nil {
 		return err
 	}
 
-	newTransaction := &portfolio.Transaction{
+	transaction := &portfolio.Transaction{
 		Date:   date,
 		Name:   name,
-		Amount: portfolio.Amount(price),
+		Amount: amount,
+		Note:   note,
 	}
-	p.AddTransaction(portfolio.VariableExpenseCategory, newTransaction)
-
+	p.AddTransaction(c, transaction)
 	return s.storage.SavePortfolio(p)
 }
